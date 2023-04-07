667ba137962a2aba668229ad5018b11a125d1d42
champ007644 <112472220+champ007644@users.noreply.github.com>
2023-04-07 14:58:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
 
+
+model = pickle.load(open('model1.pk1','rb'))
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    age = request.form.get('age')
+    sex = request.form.get('sex')
+    cp = request.form.get('cp')
+    trestbps = request.form.get('trestbps')
+    chol = request.form.get('chol')
+    fbs = request.form.get('fbs')
+    restecg = request.form.get('restecg')
+    thalach = request.form.get('thalach')
+    exang = request.form.get('exang')
+    oldpeak = request.form.get('oldpeak')
+    slope = request.form.get('slope')
+    ca = request.form.get('ca')
+    thal = request.form.get('thal')
+
+
+
+    input_query = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]],dtype=float)
+    result = model.predict(input_query)[0]
+
+
+    return jsonify({'target':str(result)})
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
